a9ead381a001d92af1de25b0e49291495742128b
Narendra-Nani <narendraraju7033@gmail.com>
2022-04-23 13:09:22 +0530

    Set_bits_count

---
+++ Set_bits_count.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	unsigned int a,count = 0,even_count = 0;
+	printf("Enter the Number in HexaDecimal format:\n");
+	scanf("%x",&a);
+	int i;
+	for(i = 0;i < 31;i++)
+	{
+		if(a & (1 << i))
+		{
+			count++;
+		}
+		else if(count % 2 == 0 && count != 0)
+		{
+			even_count++;
+			count = 0;
+		}
+		
+         }
+	 printf("No of Ones in even_count is %d:\n",even_count);
+
+}
